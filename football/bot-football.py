16a117fa20bf93bf569cd86e0eb0b28d75b7fb9c
Asker Kakhramanov <asker.kakhramanov@me.com>
2023-04-25 15:15:19 +0300

    modified football/bot-football.py

--- football/bot-football.py
+++ football/bot-football.py
@@ -59,7 +59,7 @@ def job():
 
 moscow_tz = pytz.timezone("Europe/Moscow")
 moscow_time = datetime.now(moscow_tz)
-moscow_time_20_30 = moscow_tz.localize(datetime.combine(moscow_time.date(), time(15, 7)), is_dst=None)
+moscow_time_20_30 = moscow_tz.localize(datetime.combine(moscow_time.date(), time(15, 18)), is_dst=None)
 utc_time_20_30 = moscow_time_20_30.astimezone(pytz.utc).strftime('%H:%M')
 
 schedule.every().tuesday.at(utc_time_20_30).do(job)
