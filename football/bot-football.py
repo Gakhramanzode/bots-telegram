caa8140d20be5f0415619e09fd259ae29904b36c
Asker Kakhramanov <asker.kakhramanov@me.com>
2023-04-28 13:00:02 +0300

    modified bot-football.yml bot-football.py

--- football/bot-football.py
+++ football/bot-football.py
@@ -4,6 +4,7 @@ import pytz
 import schedule
 import time as t
 import os
+from ics import Calendar, Event
 
 TOKEN = os.environ.get('football_TOKEN')
 CHAT_ID = os.environ.get('football_CHAT_ID')
@@ -47,22 +48,43 @@ def send_message(text):
     data = {"chat_id": CHAT_ID, "text": text, "parse_mode": "Markdown"}
     requests.post(url, data=data)
 
+def create_ics_file(match, date):
+    cal = Calendar()
+    event = Event()
+    event.name = match
+    event.begin = date
+    cal.events.add(event)
+    file_path = f"{match}.ics"
+    with open(file_path, 'w') as f:
+        f.writelines(cal)
+    return file_path
+
+def send_ics_file(file_path):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendDocument"
+    files = {'document': open(file_path, 'rb')}
+    data = {"chat_id": CHAT_ID}
+    requests.post(url, files=files, data=data)
+
 def job():
     message = ""
     for team_id, team_name in TEAMS.items():
         matches = get_upcoming_matches(team_id)
         if matches:
             message += f"Upcoming *{team_name}* matches within the next week:\n" + "\n".join([f"**{match}**" for match in matches]) + "\n\n"
+            for match in matches:
+                match_date = datetime.strptime(match.split(" on ")[-1], "%d %B %Y %H:%M (Moscow time)").strftime("%Y-%m-%d %H:%M:%S")
+                file_path = create_ics_file(match, match_date)
+                send_ics_file(file_path)
         else:
             message += f"No upcoming **{team_name}** matches within the next week found.\n\n"
     send_message(message)
 
 moscow_tz = pytz.timezone("Europe/Moscow")
 moscow_time = datetime.now(moscow_tz)
-moscow_time_20_30 = moscow_tz.localize(datetime.combine(moscow_time.date(), time(19, 30)), is_dst=None)
+moscow_time_20_30 = moscow_tz.localize(datetime.combine(moscow_time.date(), time(13, 3)), is_dst=None)
 utc_time_20_30 = moscow_time_20_30.astimezone(pytz.utc).strftime('%H:%M')
 
-schedule.every().monday.at(utc_time_20_30).do(job)
+schedule.every().friday.at(utc_time_20_30).do(job)
 
 if os.environ.get('CI'):
     exit(0)
